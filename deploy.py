216f0133db96384766241e0375e4aff2e1b987da
Hao Xiangpeng <3150102255@zju.edu.cn>
2017-05-18 14:43:27 +0800

    reorganize pwa

--- deploy.py
+++ deploy.py
@@ -1,5 +1,7 @@
 from subprocess import call
 
 call(['npm','run','build'])
-call(['rsync','-avzhe','ssh','dist/index.html','root@101.200.46.157:/usr/share/nginx/frontend'])
-call(['rsync','-avzhe','ssh','dist/static','root@101.200.46.157:/usr/share/nginx/frontend'])
+call(['cp','dist/static/sw.js','dist/'])
+call(['cp','dist/static/serviceworker-cache-polyfill.js','dist/'])
+# call(['rsync','-avzhe','ssh','dist/index.html','root@101.200.46.157:/usr/share/nginx/frontend'])
+# call(['rsync','-avzhe','ssh','dist/static','root@101.200.46.157:/usr/share/nginx/frontend'])
